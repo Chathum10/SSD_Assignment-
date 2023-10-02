218c2803be3231db8b61c587805a66ff444ca96b
Pawani Muthusala Perera <89269937+MuthusalaPerera@users.noreply.github.com>
2023-10-02 21:53:59 +0530

    Update projects.js

--- backend/models/projects.js
+++ backend/models/projects.js
@@ -1,68 +1,83 @@
 const mongoose = require('mongoose');
 
+// Define the Projects Schema
 const projectsSchema = new mongoose.Schema({
-
-    cName:{
-        type:String,
-        required:true
+    // Client Name
+    cName: {
+        type: String,
+        required: true
     },
-    email:{
-        type:String,
-        required:true
+    // Client Email Address
+    email: {
+        type: String,
+        required: true
     },
-    contactNo:{
-        type:String,
-        required:true
+    // Client Contact Number
+    contactNo: {
+        type: String,
+        required: true
     },
-    description:{
-        type:String,
+    // Project Description
+    description: {
+        type: String,
     },
-    projectId:{
-        type:String,
-        required:true
+    // Project ID
+    projectId: {
+        type: String,
+        required: true
     },
-    dept:{
-        type:String,
-        required:true
+    // Department responsible for the Project
+    dept: {
+        type: String,
+        required: true
     },
-    pLevel:{
-        type:String,
-        required:true
+    // Project Level (e.g., High Priority, Low Priority)
+    pLevel: {
+        type: String,
+        required: true
     },
-    sDate:{
-        type:String,
-        required:true
+    // Start Date of the Project
+    sDate: {
+        type: String,
+        required: true
     },
-    eDate:{
-        type:String,
-        required:true
+    // End Date of the Project
+    eDate: {
+        type: String,
+        required: true
     },
-    remarks:{
-        type:String,
+    // Remarks (Additional Information)
+    remarks: {
+        type: String,
     },
-
     
-    //calculation part (project progress calculation)
-    t1:{
-        type:String,
+    // Calculation part for Project Progress
+    // Task 1 Progress
+    t1: {
+        type: String,
     },
-    t2:{
-        type:String,
+    // Task 2 Progress
+    t2: {
+        type: String,
     },
-    t3:{
-        type:String,
+    // Task 3 Progress
+    t3: {
+        type: String,
     },
-    t4:{
-        type:String,
+    // Task 4 Progress
+    t4: {
+        type: String,
     },
-    t5:{
-        type:String,
+    // Task 5 Progress
+    t5: {
+        type: String,
     },
-    progress:{
-        type:String,
+    // Overall Project Progress (defaulted to "Status Pending")
+    progress: {
+        type: String,
         default: "Status Pending",
     },
-
 });
 
-module.exports = mongoose.model('projects',projectsSchema);
+// Export the Projects model with the defined schema
+module.exports = mongoose.model('projects', projectsSchema);
