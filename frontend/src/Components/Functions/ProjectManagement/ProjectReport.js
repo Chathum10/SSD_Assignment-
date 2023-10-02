37a8a1c509fcc0664de51d80c2441b2fb5b5e782
Pramoda Piyumali <pramodapiyumali@gmail.com>
2023-10-02 23:56:54 +0530

    Update ProjectReport.js

--- frontend/src/Components/Functions/ProjectManagement/ProjectReport.js
+++ frontend/src/Components/Functions/ProjectManagement/ProjectReport.js
@@ -13,6 +13,7 @@ export default class ProjectReport extends Component {
     };
   }
 
+  
   createPdf = (pdfBody) => {
     var doc = new jsPDF();
     var totalPagesExp = "{total_pages_count_string}"; //placeholder for total number of pages
