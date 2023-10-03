a4356593125914f90c7f758a02f4d5ec0b8681ff
Chathum Adhihetty <89435818+ashen910@users.noreply.github.com>
2023-10-03 20:36:44 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,7 @@ const helmet = require("helmet"); // Import the helmet middleware
 const rateLimit = require("express-rate-limit");
 
 // Connect DB
+
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
