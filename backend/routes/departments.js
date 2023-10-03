584bcdc3d1e4576c1406d84b69f0accb3e632387
Chathum Adhihetty <89435818+ashen910@users.noreply.github.com>
2023-10-03 20:36:20 +0530

    Update departments.js

--- backend/routes/departments.js
+++ backend/routes/departments.js
@@ -1,97 +1,89 @@
-const express = require('express');
-const departments = require('../models/departments');
+const express = require("express");
+const departments = require("../models/departments");
 
 const router = express.Router();
 
-
 //Save a departments Info
-router.post('/departments/save',(req,res)=>{
+router.post("/departments/save", (req, res) => {
+  let newPost = new departments(req.body);
 
-    let newPost = new departments(req.body);
-    
-    newPost.save((err) =>{
-        if(err){
-            return res.status(400).json({
-                error:err
-            });
-        }
-        return res.status(200).json({
-            success:"Posts saved successfully"
-        });
+  newPost.save((err) => {
+    if (err) {
+      return res.status(400).json({
+        error: err,
+      });
+    }
+    return res.status(200).json({
+      success: "Posts saved successfully",
     });
+  });
 });
-
-
 
 //get departments Info
-router.get('/departments',(req,res) =>{
-    departments.find().exec((err,posts) =>{
-        if(err){
-            return res.status(400).json({
-                error:err
-            });
-        }
-        return res.status(200).json({
-            success:true,
-            existingPosts:posts
-        });
+
+router.get("/departments", (req, res) => {
+  departments.find().exec((err, posts) => {
+    if (err) {
+      return res.status(400).json({
+        error: err,
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      existingPosts: posts,
     });
+  });
 });
-
-
 
 //get a specific departments Info
-router.get("/departments/:id",(req,res) =>{
+router.get("/departments/:id", (req, res) => {
+  let postId = req.params.id;
 
-    let postId = req.params.id;
+  departments.findById(postId, (err, post) => {
+    if (err) {
+      return res.status(400).json({ success: false, err });
+    }
 
-    departments.findById(postId,(err,post) =>{
-        if(err){
-            return res.status(400).json({success:false, err})
-        }
-
-        return res.status(200).json({
-            success:true,
-            post
-        });
+    return res.status(200).json({
+      success: true,
+      post,
     });
-
+  });
 });
-
 
 //update a departments Info
-router.put('/departments/update/:id',(req,res)=>{
-    departments.findByIdAndUpdate(
-        req.params.id,
-        {
-            $set:req.body
-        },
-        (err,post)=>{
-            if(err){
-                return res.status(400).json({error:err});
-            }
+router.put("/departments/update/:id", (req, res) => {
+  departments.findByIdAndUpdate(
+    req.params.id,
+    {
+      $set: req.body,
+    },
+    (err, post) => {
+      if (err) {
+        return res.status(400).json({ error: err });
+      }
 
-            return res.status(200).json({
-                success:"Updated Successfully"
-            });
-        }
-    );
+      return res.status(200).json({
+        success: "Updated Successfully",
+      });
+    }
+  );
 });
 
-
-
 //Delete a departments Info
-router.delete('/departments/delete/:id',(req,res) =>{
-    departments.findByIdAndRemove(req.params.id).exec((err,deletePost) =>{
+router.delete("/departments/delete/:id", (req, res) => {
+  departments.findByIdAndRemove(req.params.id).exec((err, deletePost) => {
+    if (err)
+      return res.status(400).json({
+        message: "Delete Unsuccesful",
+        err,
+      });
 
-        if(err) return res.status(400).json({
-            message:"Delete Unsuccesful",err
-        });
-
-        return res.json({
-            message:"Delete Successful",deletePost
-        });
+    return res.json({
+      message: "Delete Successful",
+      deletePost,
     });
+  });
 });
 
 module.exports = router;
