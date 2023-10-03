bb76c34bb6ae898f058e6c8d09bb90bc088a7f9f
Chathum Adhihetty <89435818+ashen910@users.noreply.github.com>
2023-10-03 20:35:22 +0530

    Update projects.js

--- backend/routes/projects.js
+++ backend/routes/projects.js
@@ -1,96 +1,89 @@
-const express = require('express');
-const projects = require('../models/projects');
+const express = require("express");
+const projects = require("../models/projects");
 
 const router = express.Router();
 
 //Save a projects Info
-router.post('/projects/save',(req,res)=>{
+router.post("/projects/save", (req, res) => {
+  let newPost = new projects(req.body);
 
-    let newPost = new projects(req.body);
-    
-    newPost.save((err) =>{
-        if(err){
-            return res.status(400).json({
-                error:err
-            });
-        }
-        return res.status(200).json({
-            success:"Posts saved successfully"
-        });
+  newPost.save((err) => {
+    if (err) {
+      return res.status(400).json({
+        error: err,
+      });
+    }
+    return res.status(200).json({
+      success: "Posts saved successfully",
     });
+  });
 });
-
 
 //get projects Info
-router.get('/projects',(req,res) =>{
-    projects.find().exec((err,posts) =>{
-        if(err){
-            return res.status(400).json({
-                error:err
-            });
-        }
-        return res.status(200).json({
-            success:true,
-            existingPosts:posts
-        });
+
+router.get("/projects", (req, res) => {
+  projects.find().exec((err, posts) => {
+    if (err) {
+      return res.status(400).json({
+        error: err,
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      existingPosts: posts,
     });
+  });
 });
-
-
 
 //get a specific projects Info
-router.get("/projects/:id",(req,res) =>{
+router.get("/projects/:id", (req, res) => {
+  let postId = req.params.id;
 
-    let postId = req.params.id;
+  projects.findById(postId, (err, post) => {
+    if (err) {
+      return res.status(400).json({ success: false, err });
+    }
 
-    projects.findById(postId,(err,post) =>{
-        if(err){
-            return res.status(400).json({success:false, err})
-        }
-
-        return res.status(200).json({
-            success:true,
-            post
-        });
+    return res.status(200).json({
+      success: true,
+      post,
     });
-
+  });
 });
-
-
 
 //update a projects Info
-router.put('/projects/update/:id',(req,res)=>{
-    projects.findByIdAndUpdate(
-        req.params.id,
-        {
-            $set:req.body
-        },
-        (err,post)=>{
-            if(err){
-                return res.status(400).json({error:err});
-            }
+router.put("/projects/update/:id", (req, res) => {
+  projects.findByIdAndUpdate(
+    req.params.id,
+    {
+      $set: req.body,
+    },
+    (err, post) => {
+      if (err) {
+        return res.status(400).json({ error: err });
+      }
 
-            return res.status(200).json({
-                success:"Updated Successfully"
-            });
-        }
-    );
+      return res.status(200).json({
+        success: "Updated Successfully",
+      });
+    }
+  );
 });
 
-
-
 //Delete a projects Info
-router.delete('/projects/delete/:id',(req,res) =>{
-    projects.findByIdAndRemove(req.params.id).exec((err,deletePost) =>{
+router.delete("/projects/delete/:id", (req, res) => {
+  projects.findByIdAndRemove(req.params.id).exec((err, deletePost) => {
+    if (err)
+      return res.status(400).json({
+        message: "Delete Unsuccesful",
+        err,
+      });
 
-        if(err) return res.status(400).json({
-            message:"Delete Unsuccesful",err
-        });
-
-        return res.json({
-            message:"Delete Successful",deletePost
-        });
+    return res.json({
+      message: "Delete Successful",
+      deletePost,
     });
+  });
 });
 
 module.exports = router;
